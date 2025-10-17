a67a5bf98720a08f0ae17ab8b54d856758fbbde5
0x414854 <0x414854@proton.me>
2025-10-18 00:25:19 +0200

    update middelware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,22 @@
-export { auth as middleware } from "@/auth";
+export const runtime = "nodejs"; // important pour éviter Edge runtime
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const isAuthPage = req.nextUrl.pathname.startsWith("/login");
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login"],
+};
